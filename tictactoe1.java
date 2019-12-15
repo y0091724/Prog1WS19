bc51a9bf635181d1a5431e5a251003a8bb9d95a8
n.schnieders <n.schnieders@tu-bs.de>
2019-12-15 02:17:54 +0100

    Feldinitialisierung mit Nummern sowie Feldausgabe und Gewinnueberpruefung hinzugefügt, Ausgabe des Gewinners sowie Setzen der "Steine" und Ueberpruefung ob richtige Eingaben gemacht wurden fehlt noch

--- tictactoe1.java
+++ tictactoe1.java
@@ -91,4 +91,53 @@ public class tictactoe {
 		System.out.println(zug + "ist dran, setze" + Zug + "auf eine Nummer");
 		return null;
 		}        
-	}
+	
+/*   public static boolean ueberpruefen (char[][] feld) {
+        for (int x = 0; x < 5; x++) {
+            if (feld[x][0] == 'X' || feld[x][0] == 'O'){
+                int zähler = 1;
+                while (feld[x][zähler] == feld[x][0] && zähler < 5) {
+                    zähler++;
+                }
+                if (zähler == 5) {
+                    return true;
+                }
+            }
+        }
+        for (int y = 0; y < 5; y++) {
+            if (feld[0][y] == 'X' || feld[0][y] == 'O'){
+                int zähler = 1;
+                while (feld[zähler][y] == feld[0][y] && zähler < 5) {
+                    zähler++;
+                }
+                if (zähler == 5) {
+                    return true;
+                }
+            }
+        }
+        if(feld[0][0] == 'X' || feld[0][0] == 'O') {
+            int x = 1;
+            int y = 1;
+            int zähler = 0;
+            while (feld[x][y] == feld[0][0] && x < 5) {
+                zähler++;
+            }
+            if (zähler == 5) {
+                return true;
+            }
+        }
+        if(feld[0][0] == 'X' || feld[0][0] == 'O') {
+            int x = 1;
+            int y = 1;
+            int zähler = 0;
+            while (feld[x][y] == feld[0][0] && x < 5) {
+                zähler++;
+            }
+            if (zähler == 5) {
+                return true;
+            }
+        }
+      return false;
+  */
+  }
+}
