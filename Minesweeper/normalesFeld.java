0b7321309ba43fa3dfef45893e58a74a5d2685d7
Colin <c.engel@tu-bs.de>
2020-01-07 18:53:01 +0100

    a

---
+++ Minesweeper/normalesFeld.java
@@ -0,0 +1,13 @@
+
+public class NormalesFeld extends Feld {
+
+    private int anzahlMinen;
+
+    NormalesFeld (int anzahlMinen) {
+        this.anzahlMinen = anzahlMinen;
+    }
+
+    public int getAnzahlMinen() {
+      return anzahlMinen;
+    }
+}
