126eab6792ea7278223cf49ac1a073a95245eb97
Colin <c.engel@tu-bs.de>
2020-01-11 16:45:34 +0100

    a

--- Minesweeper/Landmine.java
+++ Minesweeper/Landmine.java
@@ -1,9 +1,14 @@
 
-class Landmine extends Feld {
+public class Landmine extends Feld {
 
-  private int anzMin = 0;
+  //private int anzMin = 0;
 
   public void setAnzMin(int x) {
   }
+
+  /*public int getAnzMin() {
+    return anzMin;
+  }
+  */
 
 }
