4727889ac27f0327696bd95f9bac6e2dd2cd1858
Colin <c.engel@tu-bs.de>
2020-01-11 17:03:44 +0100

    a

--- Minesweeper/Spielfeld.java
+++ Minesweeper/Spielfeld.java
@@ -13,10 +13,6 @@ public class Spielfeld {
         erschaffeFeld();
     }
 
-    public int getSpielfeldLength() {
-      return spielfeld.length;
-    }
-
     public Feld[][] getSpielfeld() {
         return spielfeld;
     }
