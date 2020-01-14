2fd51fe95a4f38c8a6c798b68ee54975c1541c89
n.schnieders <n.schnieders@tu-bs.de>
2020-01-14 14:29:10 +0100

    Fehlerbehebung

--- Minesweeper/coordinates.java
+++ Minesweeper/coordinates.java
@@ -2,28 +2,28 @@ import java.util.Scanner
 	
 
 public class coordinates {
-  	public char[] coords = new char[1];
-	Scanner CoordsSc = new Scanner(System.in);
-  	private String xCoords;
+  	public string[] coords = new string[1];
+	Scanner xCoordsSc = new Scanner(System.in);
+	Scanner yCoordsSc = new Scanner(System.in);
+  	private int xCoords;
 
   	public void getcoordinates() {
-  	System.out.println ("Geben Sie Ihre Koordinaten in der Form A0 an (A-J, 0-9)");
-	String xCoords = CoordsSc.nextLine();
-	coords[0] = xCoords.charAt(0);
-	coords[1] = xCoords.charAt(1);
+  	System.out.println ("Geben Sie Ihre X-Koordinate (A-J) an");
+	coords[0] = xCoordsSc.nextLine();
+	System.out.println ("Geben Sie Ihre Y-Koordinate (0-9) an"); 
+	coords[1] = yCoordsSc.nextLine();
 		}
 
 
 
 	public static boolean checkInput() {
-	if (Integer.valueOf(coords[0]) < 65 || Integer.valueOf(coords[0] > 74) {
-	System.out.println("Deine Eingaben sind nicht korrekt (A-J,0-9)");
-	return false;			
+	if (coords[0] < 0 || coords[1] > 9) {
+	System.out.println("Etwas stimmt mit deinen Eingaben nicht");
+	return false;
 	}
-	if (Integer.valueOf(coords[1]) < 48 || Integer.valueOf(coords[1] > 57) {
-	System.outprintln("Deine Eingaben sind nicht korrekt(A-J,0-9)");
+	if (coords 0 > 9 || coords[1] < 0) {
+	System.out.println("Etwas stimmt mit deinen Eingaben nicht");
 	return false;
 	}
 	return true;
-	} 
-	
+	}
