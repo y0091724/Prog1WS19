8980307d2c4f4ff8997a3434618f6f3c5fea055b
Colin <c.engel@tu-bs.de>
2020-01-15 15:51:30 +0100

    a

--- Minesweeper/main.java
+++ Minesweeper/main.java
@@ -2,8 +2,16 @@
 public class main {
 
   public static void main(String args[]) {
-    Spielverlauf spiel = new Spielverlauf(10, 23, 5);
-    spiel.ausgabe();
+      Spielverlauf spiel = new Spielverlauf(10, 23, 5);
+      spiel.ausgabeVoll();
+      spiel.ausgabe();
+      while(spiel.spielzug() && !spiel.gewinnerUeberpruefen()) {
+          spiel.ausgabe();
+      }
+      if(spiel.gewinnerUeberpruefen()) {
+        System.out.println("Sie haben gewonnen!!");
+      }
+      System.out.println("Sie haben verloren, weil sie ein Feld mit Mine betreten haben.");
   }
 
 }
