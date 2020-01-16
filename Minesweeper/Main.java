a9567aa8534e400592d0cbda533f866f0f6af62d
Colin <c.engel@tu-bs.de>
2020-01-16 16:54:45 +0100

    a

--- Minesweeper/Main.java
+++ Minesweeper/Main.java
@@ -8,10 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Spielverlauf spiel = new Spielverlauf(10, 23, 5);
         spiel.ausgabeVoll();
+        System.out.println();
         spiel.ausgabe();
         while (spiel.spielzug() && !spiel.gewinnerUeberpruefen()) {
             spiel.ausgabe();
-            System.println();
+            System.out.println();
         }
         if (spiel.gewinnerUeberpruefen()) {
             System.out.println("Sie haben gewonnen!!");
