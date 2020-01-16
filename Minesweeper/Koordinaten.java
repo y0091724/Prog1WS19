6f649c7d7c81419ebaee97ff79fa5658bdc8f7d7
Colin <c.engel@tu-bs.de>
2020-01-16 16:54:20 +0100

    a

--- Minesweeper/Koordinaten.java
+++ Minesweeper/Koordinaten.java
@@ -1,4 +1,4 @@
-/** Klasse zum Speichern und Weitergeben der Koordinateninformation
+/** Klasse zum Speichern und Ueberpruefen der Koordinateninformation
 *@author Colin Engel y0099850
 *@author Nico Schnieders y001724
 *@version 1.4
@@ -7,18 +7,21 @@ public class Koordinaten {
 
     private int spalte;
     private int reihe;
+
 /**Konstruktor um die Zeilen/Spalteninformationen zu importieren
-*@param spalte Spalteninformation 
+*@param spalte Spalteninformation
 *@param reihe Zeileninformation
 */
-    public Koordinaten(char spalte, int reihe) {  
+
+    public Koordinaten(char spalte, int reihe) {
         this.spalte =  (int) spalte - 65;
         this.reihe = reihe;
     }
+
 /**Methode um zu Ueberpruefen, ob die Eingaben den Anforderungen entsprechen
 *@return false falls Eingaben falsch sind, ansonsten true
 */
-    public boolean korrektheit() { 
+    public boolean korrektheit() {
         if (reihe <= 9 && reihe >= 0 && spalte <= 9 && spalte >= 0) {
             return true;
         }
