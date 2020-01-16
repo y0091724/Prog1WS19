91f861811809bfc815642dd12ce9b22d9afc9e5b
Colin <c.engel@tu-bs.de>
2020-01-16 16:52:24 +0100

    a

--- Minesweeper/Feld.java
+++ Minesweeper/Feld.java
@@ -1,27 +1,33 @@
 /** abstrakte Klasse für die einzelnen Feldtypen, da per Interface keine Attribute übergeben werden
-*/ 
+*/
 abstract public class Feld {
 
     private boolean aufgedeckt;
     protected int anzMin = 0;
 
-/**Methode um die Anzahl Minen zu veraendern
+/**Methode um die Anzahl Minen in der Naehe zu veraendern
 @param x anzahl der minen
 */
-    abstract public void setAnzMin(int x); 
-/**Methode um die Anzahl der Minen zu exportieren
-@return Anzahl der Minen
-*/    
+    abstract public void setAnzMin(int x);
+
+/**Methode um die Anzahl der Minen zurueckzugeben
+@return Anzahl der Minen in der Naehe
+*/
     abstract public int getAnzMin();
-/**Methode um die Anzahl der aufgedeckten Felder zu exportieren
+
+/**Methode um den Status vom Feld, aufgedeckt oder nicht aufgedeckt, zurueckzugeben
 *@return aufgedeckte Anahl Felder
 */
     public boolean getAufgedeckt() {
-        return aufgedeckt; x
+        return aufgedeckt;
     }
+
+/**Methode um den Status vom Feld, aufgedeckt oder nicht aufgedeckt, zu ändern
+*@param x true fuer aufgedeckt und false für unaufgedeckt
+*/
 
     public void setAufgedeckt(boolean x) {
         aufgedeckt = x;
     }
+
 }
- 
